4fcb136d9ea93b65aaa8ffd3944170414b177dbe
ha-jinwoo <gkwlsdn95@naver.com>
2021-10-18 04:20:52 +0900

    Add algo 2021-10-18

---
+++ programmers/문자열내p와y의개수.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool solution(string s)
+{
+    int cnt1 = 0, cnt2 = 0;
+    for(int i = 0; i<s.size(); i++){
+        if(s[i] == 'p' || s[i]== 'P')cnt1++;
+        else if(s[i]=='y' || s[i]=='Y')cnt2++;
+    }
+    return cnt1==cnt2;
+}
